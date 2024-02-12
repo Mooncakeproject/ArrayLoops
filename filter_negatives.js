88279665d6e066b632aa67b8c99c80d62283552d
Mooncakeproject <ao.carey@hotmail.com>
2024-02-12 11:41:16 +0700

    created_return_positives

---
+++ filter_negatives.js
@@ -0,0 +1,6 @@
+// Write a JavaScript function named filterOutNegativeNumbers.
+// This function should take an array of numbers and return a new array with all negative numbers filtered out, leaving only positive numbers and zeros.
+function filterOutNegativeNumbers(numbers) {
+    return positiveNumbers = numbers.filter (number => number > 0)
+}
+    console.log(filterOutNegativeNumbers([1,2,3]));
